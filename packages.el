e05dc6ae2fe74c72a3d275d31a8a7d6a329511e1
Hikaru NASHIDA <nashida@xcoo.jp>
2019-01-15 11:00:53 +0900

    Add melpa-stable repository

--- packages.el
+++ packages.el
@@ -1,7 +1,7 @@
 (defvar repositories
-  '(("melpa"     . "https://melpa.org/packages/")
-    ("marmalade" . "https://marmalade-repo.org/packages/")
-    ("gnu"       . "https://elpa.gnu.org/packages/")))
+  '(("melpa-stable" . "https://stable.melpa.org/packages/")
+    ("melpa"        . "http://melpa.org/packages/")
+    ("gnu"          . "http://elpa.gnu.org/packages/")))
 
 (defvar packages
   '(darcula-theme
